5f4cb1f68953857c47c61a72096f4d6802fd7fbf
Vincent Odine <vodine@smu.edu>
2025-07-10 13:20:59 -0500

    added new args

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ def wait_for_sql_and_extract():
 def main():
     parser = argparse.ArgumentParser(description="Accelo Sync Agent CLI")
     parser.add_argument("--sql_dump", action="store_true", help="Trigger full SQL export")
+    parser.add_argument("--email_side", action="store_true", help="Start from email side")
     parser.add_argument("--unzip", action="store_true", help="Unzip latest SQL export")
+    parser.add_argument("--cleanup", action="store_true", help="Run cleanup and SQL insert")
     args = parser.parse_args()
 
     if args.sql_dump:
@@ -27,8 +29,15 @@ def main():
         trigger_sql_export()
         wait_for_sql_and_extract()
     
-    elif args.unzip:
+    elif args.email_side:
         wait_for_sql_and_extract()
+        
+    elif args.unzip:
+        unzip_latest_sql()
+        run_clean_up()
+        
+    elif args.cleanup:
+        run_clean_up()
                 
 if __name__ == "__main__":
     main()
