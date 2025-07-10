7b816955c72d9eed813c29a78550e5a773879caa
Vincent Odine <vodine@smu.edu>
2025-07-10 13:09:09 -0500

    added new parser

--- main.py
+++ main.py
@@ -6,22 +6,29 @@ from sql_watchdog import run_watchdog
 
 INTERVAL = 300
 
+def wait_for_sql_and_extract():
+    success = False
+    while not success:
+        success = run_watchdog()
+        if not success:
+            time.sleep(INTERVAL)
+    if success:
+        unzip_latest_sql()
+        run_clean_up()
+
 def main():
     parser = argparse.ArgumentParser(description="Accelo Sync Agent CLI")
     parser.add_argument("--sql_dump", action="store_true", help="Trigger full SQL export")
+    parser.add_argument("--unzip", action="store_true", help="Unzip latest SQL export")
     args = parser.parse_args()
 
     if args.sql_dump:
         login_and_save_cookies()
         trigger_sql_export()
-        success = False
-        while not success:
-            success = run_watchdog()
-            if not success:
-                time.sleep(INTERVAL)
-        if success:
-            unzip_latest_sql()
-            run_clean_up()
+        wait_for_sql_and_extract()
+    
+    elif args.unzip:
+        wait_for_sql_and_extract()
                 
 if __name__ == "__main__":
     main()
